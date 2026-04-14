2cb353d792f5cf1d1f8bf56686a36b989589743b
reinhardt7177-lab <reinhardt7177@gmail.com>
2026-04-14 13:58:34 +0900

    chore: add firebase config for production

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 // 파이어베이스 홈페이지(console.firebase.google.com)에서 프로젝트를 생성하신 뒤,
 // 제공받으신 firebaseConfig 설정값으로 아래를 덮어쓰기 해주세요!
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCMeXU2wrPoYkB9DfSddpgX0LY8p9fgGU4",
+  authDomain: "mumueglish800.firebaseapp.com",
+  projectId: "mumueglish800",
+  storageBucket: "mumueglish800.firebasestorage.app",
+  messagingSenderId: "467405238731",
+  appId: "1:467405238731:web:97e8daab35f2c110f3464e",
+  measurementId: "G-HZKXK7GZ34"
 };
 
 // 키값이 올바르게 들어왔는지 확인하는 안전장치
